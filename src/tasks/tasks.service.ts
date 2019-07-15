1327b4574943a9e5726ca9f6f63c81db1c538052
lucas teisseire <teisseire@MacBook-Air-de-lucas.local>
2019-07-15 10:09:11 +0200

    add query to get tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model'
 import * as uuid from 'uuid/v1'
-import { CreateTaskDto } from 'dist/tasks/dto/create-task-dto';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
 @Injectable()
 export class TasksService {
     private tasks: Task[] = []
 
     getAllTasks(): Task[] {
         return this.tasks
+    }
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto
+        console.log(search)
+        let tasks = this.getAllTasks()
+        if(status) {
+            tasks = tasks.filter(task => task.status === status)
+        }
+        if(search) {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) ||
+                task.description.includes(search)
+            )
+        }
+        return tasks
     }
     getTaskById(id: string): Task {
         return this.tasks.find(task => {
